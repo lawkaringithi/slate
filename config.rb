9473a3ce29278897da6ba9372a47dd5277f92155
Law <lawkaringithi@gmail.com>
2018-09-23 08:40:41 +0300

    Add middleman-livereload support

--- config.rb
+++ config.rb
@@ -20,6 +20,9 @@ set :js_dir, 'javascripts'
 set :images_dir, 'images'
 set :fonts_dir, 'fonts'
 
+# Activate live-reload
+activate :livereload
+
 # Activate the syntax highlighter
 activate :syntax
 ready do
